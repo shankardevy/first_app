b0f68e03ee9b1c777277b76c26034e22cf52a9b3
Shankar Dhanasekaran <shankar@opendrops.com>
2013-04-03 22:55:44 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'e85b501a04ab358ef7df99f47acb5de6926f799c2d399d2f470821de792496f3881c8747743f2c44314c6c202b81c78f959ed6b7b4184273cbc54936193b3a12'
